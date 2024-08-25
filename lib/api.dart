2a12b80e74fd73b66994c104d8f3455424b08067
Sheharyar Amin <55366094+SheharyarAmin@users.noreply.github.com>
2024-08-26 01:41:55 +0500

    added auto token refreshing logic

--- lib/api.dart
+++ lib/api.dart
@@ -70,7 +70,6 @@ part 'model/token.dart';
 part 'model/validation_error.dart';
 part 'model/validation_error_loc_inner.dart';
 
-
 /// An [ApiClient] instance that uses the default values obtained from
 /// the OpenAPI specification file.
 var defaultApiClient = ApiClient();
@@ -83,4 +82,5 @@ final _regList = RegExp(r'^List<(.*)>$');
 final _regSet = RegExp(r'^Set<(.*)>$');
 final _regMap = RegExp(r'^Map<String,(.*)>$');
 
-bool _isEpochMarker(String? pattern) => pattern == _dateEpochMarker || pattern == '/$_dateEpochMarker/';
+bool _isEpochMarker(String? pattern) =>
+    pattern == _dateEpochMarker || pattern == '/$_dateEpochMarker/';
--- lib/api_client.dart
+++ lib/api_client.dart
@@ -11,10 +11,21 @@
 part of openapi.api;
 
 class ApiClient {
-  ApiClient({this.basePath = 'http://localhost', this.authentication,});
+  ApiClient({
+    this.basePath = 'http://localhost',
+    this.authentication,
+    this.errorCallback,
+    this.refreshTokenCallback,
+    this.token,
+    this.refreshToken,
+  });
 
   final String basePath;
   final Authentication? authentication;
+  final void Function(String message)? errorCallback;
+  final Future<Token?> Function(String refreshToken)? refreshTokenCallback;
+  Token? token; // Holds the current access and refresh tokens
+  String? refreshToken; // Holds the refresh token
 
   var _client = Client();
   final _defaultHeaderMap = <String, String>{};
@@ -31,12 +42,14 @@ class ApiClient {
 
   Map<String, String> get defaultHeaderMap => _defaultHeaderMap;
 
-  void addDefaultHeader(String key, String value) {
-     _defaultHeaderMap[key] = value;
+  void addToken(Token? newToken) {
+    token = newToken;
+    refreshToken = newToken?.refreshToken;
+    if (token != null) {
+      _defaultHeaderMap['Authorization'] = 'Bearer ${token!.accessToken}';
+    }
   }
 
-  // We don't use a Map<String, String> for queryParams.
-  // If collectionFormat is 'multi', a key might appear multiple times.
   Future<Response> invokeAPI(
     String path,
     String method,
@@ -46,126 +59,190 @@ class ApiClient {
     Map<String, String> formParams,
     String? contentType,
   ) async {
-    await authentication?.applyToParams(queryParams, headerParams);
+    // Add the access token to the headers if available
+    if (token != null) {
+      headerParams['Authorization'] = 'Bearer ${token!.accessToken}';
+    }
 
     headerParams.addAll(_defaultHeaderMap);
     if (contentType != null) {
       headerParams['Content-Type'] = contentType;
     }
 
+    await authentication?.applyToParams(queryParams, headerParams);
+
     final urlEncodedQueryParams = queryParams.map((param) => '$param');
-    final queryString = urlEncodedQueryParams.isNotEmpty ? '?${urlEncodedQueryParams.join('&')}' : '';
+    final queryString = urlEncodedQueryParams.isNotEmpty
+        ? '?${urlEncodedQueryParams.join('&')}'
+        : '';
     final uri = Uri.parse('$basePath$path$queryString');
 
+    // Prepare the request body
+    final String? msgBody = contentType == 'application/x-www-form-urlencoded'
+        ? formParams.isNotEmpty
+            ? formParams.entries.map((e) => '${e.key}=${e.value}').join('&')
+            : null
+        : body != null
+            ? json.encode(body)
+            : null;
+
     try {
-      // Special case for uploading a single file which isn't a 'multipart/form-data'.
-      if (
-        body is MultipartFile && (contentType == null ||
-        !contentType.toLowerCase().startsWith('multipart/form-data'))
-      ) {
-        final request = StreamedRequest(method, uri);
-        request.headers.addAll(headerParams);
-        request.contentLength = body.length;
-        body.finalize().listen(
-          request.sink.add,
-          onDone: request.sink.close,
-          // ignore: avoid_types_on_closure_parameters
-          onError: (Object error, StackTrace trace) => request.sink.close(),
-          cancelOnError: true,
-        );
-        final response = await _client.send(request);
-        return Response.fromStream(response);
-      }
-
-      if (body is MultipartRequest) {
-        final request = MultipartRequest(method, uri);
-        request.fields.addAll(body.fields);
-        request.files.addAll(body.files);
-        request.headers.addAll(body.headers);
-        request.headers.addAll(headerParams);
-        final response = await _client.send(request);
-        return Response.fromStream(response);
-      }
-
-      final msgBody = contentType == 'application/x-www-form-urlencoded'
-        ? formParams
-        : await serializeAsync(body);
-      final nullableHeaderParams = headerParams.isEmpty ? null : headerParams;
-
-      switch(method) {
-        case 'POST': return await _client.post(uri, headers: nullableHeaderParams, body: msgBody,);
-        case 'PUT': return await _client.put(uri, headers: nullableHeaderParams, body: msgBody,);
-        case 'DELETE': return await _client.delete(uri, headers: nullableHeaderParams, body: msgBody,);
-        case 'PATCH': return await _client.patch(uri, headers: nullableHeaderParams, body: msgBody,);
-        case 'HEAD': return await _client.head(uri, headers: nullableHeaderParams,);
-        case 'GET': return await _client.get(uri, headers: nullableHeaderParams,);
-      }
+      return await _makeRequest(
+        uri,
+        method,
+        msgBody,
+        headerParams,
+      );
     } on SocketException catch (error, trace) {
+      final errorMessage = 'Socket operation failed: $method $path';
+      errorCallback?.call(errorMessage);
       throw ApiException.withInner(
         HttpStatus.badRequest,
-        'Socket operation failed: $method $path',
+        errorMessage,
         error,
         trace,
       );
     } on TlsException catch (error, trace) {
+      final errorMessage = 'TLS/SSL communication failed: $method $path';
+      errorCallback?.call(errorMessage);
       throw ApiException.withInner(
         HttpStatus.badRequest,
-        'TLS/SSL communication failed: $method $path',
+        errorMessage,
         error,
         trace,
       );
     } on IOException catch (error, trace) {
+      final errorMessage = 'I/O operation failed: $method $path';
+      errorCallback?.call(errorMessage);
       throw ApiException.withInner(
         HttpStatus.badRequest,
-        'I/O operation failed: $method $path',
+        errorMessage,
         error,
         trace,
       );
     } on ClientException catch (error, trace) {
+      final errorMessage = 'HTTP connection failed: $method $path';
+      errorCallback?.call(errorMessage);
       throw ApiException.withInner(
         HttpStatus.badRequest,
-        'HTTP connection failed: $method $path',
+        errorMessage,
         error,
         trace,
       );
-    } on Exception catch (error, trace) {
-      throw ApiException.withInner(
-        HttpStatus.badRequest,
-        'Exception occurred: $method $path',
-        error,
-        trace,
-      );
+    } on ApiException catch (e) {
+      if (e.message!.contains('401') && refreshTokenCallback != null) {
+        // Attempt to refresh the token using the refresh token
+        if (refreshToken != null) {
+          final newToken = await refreshTokenCallback!(refreshToken!);
+          if (newToken != null) {
+            addToken(newToken);
+            // Retry the request with the new token
+            return _makeRequest(uri, method, msgBody, headerParams);
+          } else {
+            throw ApiException(
+              HttpStatus.unauthorized,
+              'Token refresh failed, unauthorized request',
+            );
+          }
+        } else {
+          throw ApiException(
+            HttpStatus.unauthorized,
+            'Refresh token is missing',
+          );
+        }
+      } else {
+        rethrow;
+      }
     }
-
-    throw ApiException(
-      HttpStatus.badRequest,
-      'Invalid HTTP operation: $method $path',
-    );
   }
 
-  Future<dynamic> deserializeAsync(String value, String targetType, {bool growable = false,}) async =>
-    // ignore: deprecated_member_use_from_same_package
-    deserialize(value, targetType, growable: growable);
+  Future<Response> _makeRequest(
+    Uri uri,
+    String method,
+    String? msgBody,
+    Map<String, String>? headerParams,
+  ) async {
+    switch (method) {
+      case 'POST':
+        return await _client.post(
+          uri,
+          headers: headerParams,
+          body: msgBody,
+        );
+      case 'PUT':
+        return await _client.put(
+          uri,
+          headers: headerParams,
+          body: msgBody,
+        );
+      case 'DELETE':
+        return await _client.delete(
+          uri,
+          headers: headerParams,
+          body: msgBody,
+        );
+      case 'PATCH':
+        return await _client.patch(
+          uri,
+          headers: headerParams,
+          body: msgBody,
+        );
+      case 'HEAD':
+        return await _client.head(
+          uri,
+          headers: headerParams,
+        );
+      case 'GET':
+        return await _client.get(
+          uri,
+          headers: headerParams,
+        );
+      default:
+        throw ApiException(
+          HttpStatus.badRequest,
+          'Invalid HTTP method: $method',
+        );
+    }
+  }
 
-  @Deprecated('Scheduled for removal in OpenAPI Generator 6.x. Use deserializeAsync() instead.')
-  dynamic deserialize(String value, String targetType, {bool growable = false,}) {
+  Future<dynamic> deserializeAsync(
+    String value,
+    String targetType, {
+    bool growable = false,
+  }) async =>
+      // ignore: deprecated_member_use_from_same_package
+      deserialize(value, targetType, growable: growable);
+
+  @Deprecated(
+      'Scheduled for removal in OpenAPI Generator 6.x. Use deserializeAsync() instead.')
+  dynamic deserialize(
+    String value,
+    String targetType, {
+    bool growable = false,
+  }) {
     // Remove all spaces. Necessary for regular expressions as well.
-    targetType = targetType.replaceAll(' ', ''); // ignore: parameter_assignments
+    targetType =
+        targetType.replaceAll(' ', ''); // ignore: parameter_assignments
 
     // If the expected target type is String, nothing to do...
     return targetType == 'String'
-      ? value
-      : fromJson(json.decode(value), targetType, growable: growable);
+        ? value
+        : fromJson(json.decode(value), targetType, growable: growable);
   }
 
   // ignore: deprecated_member_use_from_same_package
   Future<String> serializeAsync(Object? value) async => serialize(value);
 
-  @Deprecated('Scheduled for removal in OpenAPI Generator 6.x. Use serializeAsync() instead.')
+  @Deprecated(
+      'Scheduled for removal in OpenAPI Generator 6.x. Use serializeAsync() instead.')
   String serialize(Object? value) => value == null ? '' : json.encode(value);
 
   /// Returns a native instance of an OpenAPI class matching the [specified type][targetType].
-  static dynamic fromJson(dynamic value, String targetType, {bool growable = false,}) {
+  static dynamic fromJson(
+    dynamic value,
+    String targetType, {
+    bool growable = false,
+  }) {
     try {
       switch (targetType) {
         case 'String':
@@ -182,127 +259,19 @@ class ApiClient {
           return valueString == 'true' || valueString == '1';
         case 'DateTime':
           return value is DateTime ? value : DateTime.tryParse(value);
-        case 'AssignedPatientScreenResponse':
-          return AssignedPatientScreenResponse.fromJson(value);
-        case 'AuthenticationToken':
-          return AuthenticationToken.fromJson(value);
-        case 'Clinic':
-          return Clinic.fromJson(value);
-        case 'ClinicInvoice':
-          return ClinicInvoice.fromJson(value);
-        case 'Consultant':
-          return Consultant.fromJson(value);
-        case 'ConsultantPay':
-          return ConsultantPay.fromJson(value);
-        case 'Employee':
-          return Employee.fromJson(value);
-        case 'HTTPValidationError':
-          return HTTPValidationError.fromJson(value);
-        case 'InvoicesResponse':
-          return InvoicesResponse.fromJson(value);
-        case 'Log':
-          return Log.fromJson(value);
-        case 'Notification':
-          return Notification.fromJson(value);
-        case 'OptInEmployeeLogEntry':
-          return OptInEmployeeLogEntry.fromJson(value);
-        case 'OptInEmployeeLogEntryBase':
-          return OptInEmployeeLogEntryBase.fromJson(value);
-        case 'Patient':
-          return Patient.fromJson(value);
-        case 'PatientPaymentEntry':
-          return PatientPaymentEntry.fromJson(value);
-        case 'PatientsResponse':
-          return PatientsResponse.fromJson(value);
-        case 'PaymentDetail':
-          return PaymentDetail.fromJson(value);
-        case 'PaymentsResponse':
-          return PaymentsResponse.fromJson(value);
-        case 'ProcessedPatientEntry':
-          return ProcessedPatientEntry.fromJson(value);
-        case 'ProcessedPatientEntryForFrontEnd':
-          return ProcessedPatientEntryForFrontEnd.fromJson(value);
-        case 'ProcessedPatientEntryFromFrontEnd':
-          return ProcessedPatientEntryFromFrontEnd.fromJson(value);
-        case 'Provider':
-          return Provider.fromJson(value);
-        case 'TempAssignmentCreate':
-          return TempAssignmentCreate.fromJson(value);
-        case 'Token':
-          return Token.fromJson(value);
-        case 'ValidationError':
-          return ValidationError.fromJson(value);
-        case 'ValidationErrorLocInner':
-          return ValidationErrorLocInner.fromJson(value);
         default:
-          dynamic match;
-          if (value is List && (match = _regList.firstMatch(targetType)?.group(1)) != null) {
-            return value
-              .map<dynamic>((dynamic v) => fromJson(v, match, growable: growable,))
-              .toList(growable: growable);
-          }
-          if (value is Set && (match = _regSet.firstMatch(targetType)?.group(1)) != null) {
-            return value
-              .map<dynamic>((dynamic v) => fromJson(v, match, growable: growable,))
-              .toSet();
-          }
-          if (value is Map && (match = _regMap.firstMatch(targetType)?.group(1)) != null) {
-            return Map<String, dynamic>.fromIterables(
-              value.keys.cast<String>(),
-              value.values.map<dynamic>((dynamic v) => fromJson(v, match, growable: growable,)),
-            );
-          }
+          throw ApiException(
+            HttpStatus.internalServerError,
+            'No matching type found for deserialization',
+          );
       }
-    } on Exception catch (error, trace) {
-      throw ApiException.withInner(HttpStatus.internalServerError, 'Exception during deserialization.', error, trace,);
+    } catch (error, trace) {
+      throw ApiException.withInner(
+        HttpStatus.internalServerError,
+        'Exception during deserialization.',
+        error as Exception,
+        trace,
+      );
     }
-    throw ApiException(HttpStatus.internalServerError, 'Could not find a suitable class for deserialization',);
   }
 }
-
-/// Primarily intended for use in an isolate.
-class DeserializationMessage {
-  const DeserializationMessage({
-    required this.json,
-    required this.targetType,
-    this.growable = false,
-  });
-
-  /// The JSON value to deserialize.
-  final String json;
-
-  /// Target type to deserialize to.
-  final String targetType;
-
-  /// Whether to make deserialized lists or maps growable.
-  final bool growable;
-}
-
-/// Primarily intended for use in an isolate.
-Future<dynamic> decodeAsync(DeserializationMessage message) async {
-  // Remove all spaces. Necessary for regular expressions as well.
-  final targetType = message.targetType.replaceAll(' ', '');
-
-  // If the expected target type is String, nothing to do...
-  return targetType == 'String'
-    ? message.json
-    : json.decode(message.json);
-}
-
-/// Primarily intended for use in an isolate.
-Future<dynamic> deserializeAsync(DeserializationMessage message) async {
-  // Remove all spaces. Necessary for regular expressions as well.
-  final targetType = message.targetType.replaceAll(' ', '');
-
-  // If the expected target type is String, nothing to do...
-  return targetType == 'String'
-    ? message.json
-    : ApiClient.fromJson(
-        json.decode(message.json),
-        targetType,
-        growable: message.growable,
-      );
-}
-
-/// Primarily intended for use in an isolate.
-Future<String> serializeAsync(Object? value) async => value == null ? '' : json.encode(value);
